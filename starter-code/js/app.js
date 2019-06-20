c70c77032c5aae0cdf09c530c01638cb0803f23a
Nicholas Paro <nichparo@amazon.com>
2019-06-20 15:33:51 -0700

    file sync

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,10 +8,16 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // Cart.prototype.addItem('jack', 2);
+  var newItem = new CartItem(product, quantity);
+  this.items.push(newItem);
+
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // console.log('this.items:', cart);
+  // localStorage.setItem('items', this.items);
 };
 
 Cart.prototype.removeItem = function(item) {
